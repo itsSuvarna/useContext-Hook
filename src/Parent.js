f22ced70aaa61b4dbf8866c17409d7e16329e8ec
itsSuvarna <117782206+itsSuvarna@users.noreply.github.com>
2023-04-08 11:26:15 +0530

    Update Parent.js

--- src/Parent.js
+++ src/Parent.js
@@ -1,1 +1,25 @@
+import React, { createContext } from "react"
+import ChildA from "./ChildA"
+
+
+const FirstName =createContext()
+const LastName = createContext()
+
+
+function Parent(){
+  
+  return(
+     <>
+         <FirstName.Provider value={"suvarna"}>
+           <LastName.Provider value={"somvanshi"}>
+              <ChildA/>
+            </LastName.Provider>
+          </FirstName.Provider>
+    </>
+  )
+}
+
+export default Parent
+//to export multiple ele use object as below
+export {FirstName,LastName}
 
