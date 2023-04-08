86b123305dee4b8bba52d2c3b042f28d7d0fbcd8
itsSuvarna <117782206+itsSuvarna@users.noreply.github.com>
2023-04-08 11:51:47 +0530

    Update ChildA.js

--- src/ChildA.js
+++ src/ChildA.js
@@ -1,10 +1,13 @@
 import React, { useContext } from "react"
 import {FirstName,LastName} from "./Parent"
 
+
 function ChildA(){
 
   const fname = useContext(FirstName)
   const lname = useContext(LastName)
+  
+  
   return(
     <>
        my Name is {fname}{lname}
